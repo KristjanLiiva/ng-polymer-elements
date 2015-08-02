91a9108e6d3eb63b5db61b810034eb4884a66494
Kristjan <liiva.kristjan+github@gmail.com>
2015-08-02 14:33:01 +0200

    An interval to wait until the polymer elements are ready. It is dirty fix for firefox and IE.

--- ng-polymer-elements.js
+++ ng-polymer-elements.js
@@ -149,148 +149,153 @@
 
           link: function (scope, element, attrs) {
 
-            var el = element[0];
+            var intervalId = window.setInterval(
+              function () {
+                if (element[0].getPropertyInfo) {
+                  clearInterval(intervalId);
+                  var el = element[0];
 
-            var observers = {}
+                  var observers = {};
 
-            scope.$on('$destroy', function () {
-              Object.keys(observers).forEach(function(key) {
-                var observer = observers[key];
-                Object.unobserve(el[key], observer);
-              });
-            });
-
-            keys.forEach(function(attr) {
-
-              // Don't create bindings for non-existent attributes
-              if(!attrs[attr]) {
-                return;
-              }
-
-              var mapped = mappings[attr];
-              var mappingType;
-              if(typeof mapped === 'function') {
-                mappingType = mapped.name === 'property' ? '=' : '&';
-                mapped = mapped(el);
-              } else {
-                mappingType = mapped.charAt(0);
-                mapped = mapped.substr(1);
-              }
-
-              if(mappingType === '&') {
-
-                // Event mapping
-
-                var fn = $parse(attrs[attr]);
-                el.addEventListener(mapped, function (e) {
-                  scope.$apply(function() {
-                    fn(scope.$parent, {$event: e});
+                  scope.$on('$destroy', function () {
+                    Object.keys(observers).forEach(function(key) {
+                      var observer = observers[key];
+                      Object.unobserve(el[key], observer);
+                    });
                   });
-                });
 
-              } else {
-
-                // Property mapping
-
-                var propertyName = mapped;
-                var propertyInfo = el.getPropertyInfo(mapped);
-                var propertyType = propertyInfo.type;
-                var readOnly = propertyInfo.readOnly;
-
-                // For object and array property types, if the element has no
-                // initial value - set it to empty object/array.
-                if(!readOnly && !el[propertyName]) {
-                  switch(propertyType) {
-                  case Array:
-                    el[propertyName] = [];
-                    break;
-                  case Object:
-                    el[propertyName] = {};
-                    break;
-                  }
-                }
-
-                // Observe changes to the array/object, and copy its content
-                // to the directive attribute.
-                var attachObserver = function() {
-                  if(!readOnly) {
-                    if(observers[propertyName]) {
-                      Object.unobserve(el[propertyName],
-                        observers[propertyName]);
-                      delete observers[propertyName];
+                  keys.forEach(function(attr) {
+                    // Don't create bindings for non-existent attributes
+                    if(!attrs[attr]) {
+                      return;
                     }
-                    switch(propertyType) {
-                    case Array:
-                    case Object:
-                      observers[propertyName] = function() {
+
+                    var mapped = mappings[attr];
+                    var mappingType;
+                    if(typeof mapped === 'function') {
+                      mappingType = mapped.name === 'property' ? '=' : '&';
+                      mapped = mapped(el);
+                    } else {
+                      mappingType = mapped.charAt(0);
+                      mapped = mapped.substr(1);
+                    }
+
+                    if(mappingType === '&') {
+
+                      // Event mapping
+
+                      var fn = $parse(attrs[attr]);
+                      el.addEventListener(mapped, function (e) {
                         scope.$apply(function() {
-                          if(!scope[attr]) {
-                            scope[attr] = propertyType === Array ? [] : {};
-                          }
-                          angular.copy(el[propertyName], scope[attr]);
+                          fn(scope.$parent, {$event: e});
                         });
-                      }
+                      });
 
-                      Object.observe(el[propertyName], observers[propertyName]);
-                      break;
-                    }
-                  }
-                };
+                    } else {
 
-                attachObserver();
+                      // Property mapping
 
-                // Copy the directive attribute value to the element's property.
-                // For arrays and objects, copy the content.
-                // The copying is deferred to the next event loop because some
-                // elements (eg. gold-cc-input) may change the property value
-                // immediately after inputting it, and we want to use only the
-                // latest value.
-                var handler = function() {
-                  setTimeout(function() {
-                    var value = scope[attr];
-                    if(propertyType != Array && propertyType != Object) {
+                      var propertyName = mapped;
+                      var propertyInfo = el.getPropertyInfo(mapped);
+                      var propertyType = propertyInfo.type;
+                      var readOnly = propertyInfo.readOnly;
 
-                      // Undefined value is ignored in order to allow binding to
-                      // values without initiallizing them with an "empty"
-                      // value. Some elements try to process the value on any
-                      // change without safety check.
-                      if(value !== undefined) {
-                        el[propertyName] = value;
-                      }
-                    } else if(value) {
-                      el[propertyName] = angular.copy(value);
-                      attachObserver();
-                    }
-                  });
-                };
-
-                if(!readOnly) {
-                  scope.$watch(attr, handler, true);
-                  handler(scope[attr]);
-                }
-
-                // When the property value changes, copy its new value to the
-                // directive attribute.
-                var eventName = propertyName.replace(/([A-Z])/g, function($1) {
-                  return '-' + $1.toLowerCase();
-                }) + '-changed';
-                el.addEventListener(eventName, function(event) {
-                  var value = el[propertyName]; //event.detail.value;
-                  el.async(function() {
-                    scope.$apply(function () {
-                      if(propertyType === Array || propertyType === Object) {
-                        scope[attr] = angular.copy(value);
-                      } else {
-                        if(scope[attr] != value) {
-                          scope[attr] = value;
+                      // For object and array property types, if the element has no
+                      // initial value - set it to empty object/array.
+                      if(!readOnly && !el[propertyName]) {
+                        switch(propertyType) {
+                        case Array:
+                          el[propertyName] = [];
+                          break;
+                        case Object:
+                          el[propertyName] = {};
+                          break;
                         }
                       }
-                    });
-                    attachObserver();
+
+                      // Observe changes to the array/object, and copy its content
+                      // to the directive attribute.
+                      var attachObserver = function() {
+                        if(!readOnly) {
+                          if(observers[propertyName]) {
+                            Object.unobserve(el[propertyName],
+                              observers[propertyName]);
+                            delete observers[propertyName];
+                          }
+                          switch(propertyType) {
+                          case Array:
+                          case Object:
+                            observers[propertyName] = function() {
+                              scope.$apply(function() {
+                                if(!scope[attr]) {
+                                  scope[attr] = propertyType === Array ? [] : {};
+                                }
+                                angular.copy(el[propertyName], scope[attr]);
+                              });
+                            }
+
+                            Object.observe(el[propertyName], observers[propertyName]);
+                            break;
+                          }
+                        }
+                      };
+
+                      attachObserver();
+
+                      // Copy the directive attribute value to the element's property.
+                      // For arrays and objects, copy the content.
+                      // The copying is deferred to the next event loop because some
+                      // elements (eg. gold-cc-input) may change the property value
+                      // immediately after inputting it, and we want to use only the
+                      // latest value.
+                      var handler = function() {
+                        setTimeout(function() {
+                          var value = scope[attr];
+                          if(propertyType != Array && propertyType != Object) {
+
+                            // Undefined value is ignored in order to allow binding to
+                            // values without initiallizing them with an "empty"
+                            // value. Some elements try to process the value on any
+                            // change without safety check.
+                            if(value !== undefined) {
+                              el[propertyName] = value;
+                            }
+                          } else if(value) {
+                            el[propertyName] = angular.copy(value);
+                            attachObserver();
+                          }
+                        });
+                      };
+
+                      if(!readOnly) {
+                        scope.$watch(attr, handler, true);
+                        handler(scope[attr]);
+                      }
+
+                      // When the property value changes, copy its new value to the
+                      // directive attribute.
+                      var eventName = propertyName.replace(/([A-Z])/g, function($1) {
+                        return '-' + $1.toLowerCase();
+                      }) + '-changed';
+                      el.addEventListener(eventName, function(event) {
+                        var value = el[propertyName]; //event.detail.value;
+                        el.async(function() {
+                          scope.$apply(function () {
+                            if(propertyType === Array || propertyType === Object) {
+                              scope[attr] = angular.copy(value);
+                            } else {
+                              if(scope[attr] != value) {
+                                scope[attr] = value;
+                              }
+                            }
+                          });
+                          attachObserver();
+                        });
+                      });
+                    }
                   });
-                });
               }
-            });
+            }, 100);
           }
         };
       }]);
